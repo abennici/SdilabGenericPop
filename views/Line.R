a0f89fcebfdccf4ca9a0d992d8cc087229e0cd4b
Bennici <alexandre-ben@hotmail.fr>
2021-01-14 11:54:33 +0100

    switch from ggplot to plotly

--- views/Line.R
+++ views/Line.R
@@ -5,32 +5,18 @@ LineUI <- function(id) {
   # Options for Spinner
   options(spinner.color="#0275D8", spinner.color.background="#ffffff", spinner.size=1)
   tabPanel(title=uiOutput(ns("title_panel")),
-       fluidRow(
-  #   #without box
-  column(6,selectInput(inputId = ns('x'), label = "Select x-axis Variable:",choices = NULL)),
-  column(6,selectInput(inputId = ns('y'), label = "Select y-axis Variable:",choices = NULL))
-      ),
-  uiOutput(ns("slider")),
-      fluidRow(
-  column(6,radioButtons(ns("SplitByColumn"),"Split by another column:",choices = c("no", "yes"),selected = "no",inline = TRUE)),
-  column(6,uiOutput(ns("colorColumn")))
-      ),
-  uiOutput(ns("number")),
-  div(plotOutput(ns('plot'),height="250px")%>%withSpinner(type = 2),  style = "font-size:80%"))
-    #with box
-  # shinydashboard::box(id =ns('box1'),
-  #     collapsible = T,
-  #     collapsed = T,
-  #     selectInput(inputId = ns('x'), label = "Select x-axis Variable:",choices = NULL),
-  #     uiOutput(ns("slider")),
-  #     radioButtons(ns("SplitByColumn"),"Split by another column:",choices = c("no", "yes"),selected = "no",inline = TRUE),
-  #     selectInput(inputId = ns('y'), label = "Select y-axis Variable:",choices = NULL),
-  #     uiOutput(ns("colorColumn")),
-  #     uiOutput(ns("number"))
-  #     )),
-  # fluidRow(
-  # div(plotOutput(ns('plot'),height="250px")%>%withSpinner(type = 2),  style = "font-size:80%")
-  # ))
+           fluidRow(
+             #   #without box
+             column(6,selectInput(inputId = ns('x'), label = "Select x-axis Variable:",choices = NULL)),
+             column(6,selectInput(inputId = ns('y'), label = "Select y-axis Variable:",choices = NULL))
+           ),
+           uiOutput(ns("slider")),
+           fluidRow(
+             column(6,radioButtons(ns("SplitByColumn"),"Split by another column:",choices = c("no", "yes"),selected = "no",inline = TRUE)),
+             column(6,uiOutput(ns("colorColumn")))
+           ),
+           uiOutput(ns("number")),
+           div(plotlyOutput(ns('plot'),height="250px")%>%withSpinner(type = 2),  style = "font-size:80%"))
 }
 
 # Function for module server logic
@@ -38,10 +24,10 @@ Line <- function(input, output, session,data,dsd,line.x,line.y,line.z,line.title
   ns <- session$ns 
   
   output$title_panel = renderText({
-           line.title()
-        })
+    line.title()
+  })
   
-   observe({
+  observe({
     dsd <- dsd()
     line.x<-line.x()
     attribute<-setdiff(as.character(dsd[dsd$MemberType=='attribute',]$MemberCode),c("geometry","aggregation_method"))
@@ -68,53 +54,48 @@ Line <- function(input, output, session,data,dsd,line.x,line.y,line.z,line.title
     variable<-as.character(dsd[dsd$MemberType=='variable',]$MemberCode)
     updateSelectInput(session, 'y', choices =variable, selected=line.y)
   }) 
-
+  
   yVarName <- reactive({
     input$y
   })
   
-
-      
   observe({
     observeEvent(input$SplitByColumn, {
       if(input$SplitByColumn == "yes"){ 
-      dsd <- dsd()
-      df<-as.data.frame(data())
-      line.z<-line.z()
-      attribute<-setdiff(as.character(dsd[dsd$MemberType=='attribute',]$MemberCode),c("geometry","aggregation_method"))
-      output$colorColumn<-renderUI({
+        dsd <- dsd()
+        df<-as.data.frame(data())
+        line.z<-line.z()
+        attribute<-setdiff(as.character(dsd[dsd$MemberType=='attribute',]$MemberCode),c("geometry","aggregation_method"))
+        output$colorColumn<-renderUI({
+          
+          selectInput(inputId = ns('z'), label = "Select Color Variable:", choices = attribute, selected = line.z)
+        })
         
-      selectInput(inputId = ns('z'), label = "Select Color Variable:", choices = attribute, selected = line.z)
-      })
-      
-      zVarName <- reactive({
-        input$z
-      })
-      print(zVarName())
-      output$number<-renderUI({
-        selectInput(inputId = ns('n'), label = "Numbers of element:", choices = seq(1,nrow(unique(subset(df,select=input$z))),by=1), selected = 10)
-      })
-      
-      nVarName <- reactive({
-        input$n
-      })
-      
+        zVarName <- reactive({
+          input$z
+        })
+        print(zVarName())
+        output$number<-renderUI({
+          nb_unique<-nrow(unique(subset(df,select=input$z)))
+          nb_select<-ifelse(nb_unique>=10,10,nb_unique)                
+          selectInput(inputId = ns('n'), label = "Numbers of element:", choices = seq(1,nb_unique,by=1), selected = nb_select)
+        })
+        
+        nVarName <- reactive({
+          input$n
+        })
+        
       }
       
-      output$plot <- renderPlot({
+      output$plot <- renderPlotly({
         df<-as.data.frame(data())
-        line.title<-line.title()
-        print("line title:")
-        print(line.title)
+
         df_rank<-df%>%
-        group_by(!! sym(input$z))%>%
-        summarise(sum = sum(!! sym(input$y)))%>%
-        mutate(rank = rank(-sum))%>%
-        filter(rank <= as.numeric(input$n))
+          group_by(!! sym(input$z))%>%
+          summarise(sum = sum(!! sym(input$y)))%>%
+          mutate(rank = rank(-sum))%>%
+          filter(rank <= as.numeric(input$n))
         names(df_rank)<-c("gr_name","sum","rank")
-        print("NB values :")
-        print(input$n)
-        print(df_rank)
         
         df <- df %>%
           filter(!! sym(input$x) %in% seq(min(input$s),max(input$s),1)) %>%
@@ -123,50 +104,92 @@ Line <- function(input, output, session,data,dsd,line.x,line.y,line.z,line.title
           summarise(var_sum = sum(!! sym(input$y)))%>%
           ungroup()
         
-      names(df)<-c("attr_name","attr_col","var_sum")
-      
-      
+        names(df)<-c("attr_name","attr_col","var_sum")
+        
+        
         print(df)
-        ggplot(df, aes(x = attr_name, y = var_sum, color= attr_col))+
-          geom_line()+
-          theme_bw()+
-          labs(title=line.caption(),x=input$x,y=input$y,color=input$z)+
-          theme(
-            axis.text.x = element_text(angle=90,vjust=0.5),
-            plot.title = element_text(hjust = 0.5)
-          ) 
-     })
+        
+        fig <- df %>% 
+          plot_ly(
+            x = ~attr_name, 
+            y = ~var_sum,
+            split=~attr_col,
+            height = 300,
+            type = 'scatter', 
+            mode = 'lines',
+            line = list(simplyfy = F),
+            text = ~paste(input$x,": ", attr_name, "<br>",input$z,": ", attr_col, "<br>",input$y,": ", var_sum), 
+            hoverinfo = 'text'
+          )
+        
+        fig <- fig %>% layout(
+          title = line.caption(),
+          legend = list(orientation = "v",
+                        font = list(size = 10),
+                        bgcolor ='rgba(0,0,0,0)',
+                        title = input$z),
+          xaxis = list(
+            titlefont = list(size = 10), 
+            tickfont = list(size = 10),
+            range = range(unique(df$attr_name)),
+            title = input$x,
+            zeroline = F
+          ),
+          yaxis = list(
+            titlefont = list(size = 10), 
+            tickfont = list(size = 10),
+            range = c(0,max(df$var_sum)+25*max(df$var_sum)/100),
+            title = input$y,
+            zeroline = F
+          ))
+       
+         fig
+           
+      })
     })
   }) 
-
+  
   observe({
     observeEvent(input$SplitByColumn, {
       if(input$SplitByColumn == "no"){  
         output$colorColumn<-renderUI({})
         output$number<-renderUI({})
         
-     output$plot <- renderPlot({
-    df<-as.data.frame(data())
-    
-    df <- df %>%
-      filter(!! sym(input$x) %in% seq(min(input$s),max(input$s),1)) %>%
-      group_by(!! sym(input$x)) %>% 
-      summarise(var_sum = sum(!! sym(input$y)))%>%
-      ungroup()
-    names(df)<-c("attr_name","var_sum")
-print(df)
-       ggplot(df, aes(x = attr_name, y = var_sum))+
-       geom_line()+
-       labs(title=line.caption(),x=input$x,y=input$y)+
-       theme_bw()+
-       theme(
-         axis.text.x = element_text(angle=90,vjust = 0.5),
-         plot.title = element_text(hjust = 0.5)
-       ) 
-      })
+        output$plot <- renderPlotly({
+          
+          df<-as.data.frame(data())
+          
+          df <- df %>%
+            filter(!! sym(input$x) %in% seq(min(input$s),max(input$s),1)) %>%
+            group_by(!! sym(input$x)) %>% 
+            summarise(var_sum = sum(!! sym(input$y)))%>%
+            stats::setNames(c("attr_name","var_sum")) %>%
+            ungroup()
+        
+          print(df)
+          
+          fig <- df %>% 
+            plot_ly(
+              x = ~attr_name, 
+              y = ~var_sum,
+              height = 300,
+              type = 'scatter', 
+              mode = 'lines',
+              fill = 'tozeroy',
+              line = list(simplyfy = F),
+              text = ~paste(input$x,": ", attr_name, "<br>",input$y,": ", var_sum), 
+              hoverinfo = 'text'
+            )
+          
+          fig <- fig %>% layout(title = line.caption(),
+                                yaxis = list(title = input$y, range = c(0,max(df$var_sum)+25*max(df$var_sum)/100), zeroline = F),
+                                xaxis = list( title = input$x, zeroline = F)
+          ) 
+          fig
+        })
       }
     }) 
-
+    
   })
 }
 ####
