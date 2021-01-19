f546c3a1d3b56206cadf86813af6309c7427f397
Bennici <alexandre-ben@hotmail.fr>
2021-01-19 16:47:38 +0100

    add hidden tab for insert module target

--- ui.R
+++ ui.R
@@ -1,15 +1,16 @@
 ui <- fluidPage(
-  setBackgroundColor(color = "white"),
-  useShinydashboard(),
-  tags$head(tags$link(rel="stylesheet", type="text/css", href="popup.css")),
+  
+setBackgroundColor(color = "white"),
+useShinydashboard(),
+useShinyjs(),
+tags$head(tags$link(rel="stylesheet", type="text/css", href="popup.css")),
   fluidRow(
     column(
       width = 4,
       tags$h4(FlagNameUI(id="name")),
-      
       mainPanel(
         tabsetPanel(id="main",type = "tabs",
-                    DataTableUI(id="table")
+            tabPanel(id="hidden","")
         )
       )
     )
