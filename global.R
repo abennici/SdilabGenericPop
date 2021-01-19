05bdb81c360d4e38c5096376d48a64baf71f75da
Bennici <alexandre-ben@hotmail.fr>
2021-01-19 16:48:08 +0100

    update source module

--- global.R
+++ global.R
@@ -24,12 +24,7 @@ options(spinner.color="#0275D8", spinner.color.background="#ffffff", spinner.siz
 #load module functions
 source("https://raw.githubusercontent.com/eblondel/OpenFairViewer/master/src/resources/shinyModule/QueryInfo.R")
 source("settings/QueryData.R")
-source("settings/Params.R")
 source("views/FlagName.R")
-#source("views/Line.R")
-#source("views/Pie.R")
-#source("views/Box.R")
-source("views/DataTable.R")
 source("ui.R")
 source("server.R")
 
