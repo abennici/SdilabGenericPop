dc71d25ba3edefee5fb06c616e785943d9071306
Bennici <alexandre-ben@hotmail.fr>
2021-07-22 10:01:14 +0200

    test pass js instruction

--- remotes/fao_aqua_env.R
+++ remotes/fao_aqua_env.R
@@ -1,6 +1,8 @@
 ###FAO Aquaculture image display and environmental enrichment Module
 ##Query Example : /?pid=aquaculture_farm_detection&layer=aquaculture_farm_detection&wfs_server=https://geoserver-sdi-lab.d4science.org/geoserver/sdilab_fisheriesatlas/ows&wfs_version=1.0.0&wms_server=https://geoserver-sdi-lab.d4science.org/geoserver/sdilab_fisheriesatlas/wms&wms_version=1.1.0&feature_geom=false&strategy=ogc_filters&geom=the_geom&x=217&y=181&width=256&height=256&bbox=-8140237.7642581295,-5165920.119625352,-8101102.005776119,-5126784.361143342&srs=EPSG:3857&geoCol=null&panel=fao_aqua_env&fao_aqua_env.title=Aquaculture-Environmental%20enrichment&fao_aqua_env.script=https://raw.githubusercontent.com/abennici/SdilabGenericPop/master/remotes/fao_aqua_env.R
 # Function for module UI
+jsCode <- "shinyjs.SwitchMapView = function(){window.parent.OFV.SwitchMapView();}"
+
 fao_aqua_env_ui <- function(id) {
   ns <- NS(id)
   
@@ -31,6 +33,12 @@ fao_aqua_env_ui <- function(id) {
              fluidRow(
                plotlyOutput(ns('graph'))
              )
+           ),
+           tabPanel("Map_interaction",
+                    fluidRow(
+                      extendShinyjs(text = jsCode),
+                      actionButton(ns("mapview"), "Switch 2D/3D view on map", class = "btn-success"),
+                    )
            )
            # tabPanel("Selection",
            #          fluidRow(
@@ -44,6 +52,11 @@ fao_aqua_env_ui <- function(id) {
 # Function for module server
 fao_aqua_env_server <- function(input, output, session,data,dsd,query) {
   ns<-session$ns
+  
+  onclick(ns("mapview"),{
+    #window.parent.OFV.SwitchMapView()
+    js$SwitchMapView()
+  })
   
   out <-reactiveValues(
     data=as.data.frame(data)
