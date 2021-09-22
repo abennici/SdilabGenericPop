4c2cfd98bfd334aec64e2710625fc4a6040eb0f3
Bennici <alexandre-ben@hotmail.fr>
2021-09-22 17:28:35 +0200

    test

--- remotes/fao_aqua_env.R
+++ remotes/fao_aqua_env.R
@@ -307,7 +307,8 @@ osm_response<-reactiveVal(NULL)
    output$message<-renderUI({
      req(input$project_them)
      if(input$project_them&nrow(osm_response())>0){x<-gsub("'","\'",as(geojson::as.geojson(osm_response()),"character"))
-     style<-if(subset(osm,id==input$interactWith)$geometry=="osm_points"){"new Style({image: new Circle({radius: 25,fill: new Fill({color: \"rgba(255, 51,57, 0.3)\",}),stroke: null,}),}),"}else{
+     style<-if(input$interactWith=="ferry_terminal"){"new Style({image: new Icon({scr:\"https://upload.wikimedia.org/wikipedia/commons/6/62/Anchor_pictogram.svg\",}),}),,"
+     }else if(subset(osm,id==input$interactWith)$geometry=="osm_points"){"new Style({image: new Circle({radius: 25,fill: new Fill({color: \"rgba(255, 51,57, 0.3)\",}),stroke: null,}),}),"}else{
        "new Style({stroke: new Stroke({color: \"rgba(255, 51,57, 1.0)\",width: 1,}),fill: new Fill({color: \"rgba(255, 51,57, 0.3)\",}),}),"}
      
      print(paste0("parent.postMessage('OFV.drawFeaturesFromGeoJSON(",x,",",style,")','*');"))
